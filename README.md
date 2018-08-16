dbc709b57d8d5451e9385e2cfcddeb1992e6a447
Sagit <ourfor@foxmail.com>
2018-08-16 11:23:15 +0800

    delere some .DS_Store

--- README.md
+++ README.md
@@ -3,8 +3,20 @@ MI6调教指南
 
 --- 
 ![bg-camera-small.jpg](https://i.loli.net/2018/08/15/5b7435e9a260b.jpg)
+--- 
 ![bg-top-small.jpg](https://i.loli.net/2018/08/15/5b7435e988976.jpg)
+--- 
 ![bg-perfor-small.jpg](https://i.loli.net/2018/08/15/5b7435e98718a.jpg)
+--- 
 ![image47.jpg](https://i.loli.net/2018/08/15/5b7435e9266eb.jpg)
 
-- 系统为官方稳定系统 MIUI10
+> 本指南将在官方MIUI的系统基础上对MI 6进行个性化，以达到稳定省电的目的。
+> 一个稳定的系统可以极大的提高学习的效率       ---- ***鲁迅***
+
+So,不修改系统才能保证原来的稳定，所以这里使用Magisk模块(一部分自制，一部分来自酷友)：
+
+# 好用的Magisk模块
+- 时间居中模块 miui_center_clocker
+- MIUI主题（整合安卓7.0开机画面）
+- 全面屏手势（包含禁用系统升级，Google相机代码）
+- mm管理器（方便recovery下面卸载模块）
--- magisk-module-template/config.sh
+++ magisk-module-template/config.sh
@@ -1,6 +1,6 @@
 ##########################################################################################
 #
-# 小米6 MIUI 10 个性化主题 magisk 模块  
+# 小米6 MIUI 10 个性化 magisk 模块  
 # made by ourfor
 # website: ourfor.top
 # 
@@ -43,8 +43,22 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************************"
-  ui_print "     MIUI 10 个性化主题 magisk 模块    "
-  ui_print "    anthor: ourfor  qq群： 863481960       "
+  ui_print "                                           "
+  ui_print "                        m""                "
+  ui_print "  mmm   m   m   m mm  mm#mm   mmm    m mm  "
+  ui_print " #" "#  #   #   #"  "   #    #" "#   #"  " "
+  ui_print " #   #  #   #   #       #    #   #   #     "
+  ui_print " "#m#"  "mm"#   #       #    "#m#"   #     "
+  ui_print "                                           "
+  ui_print "*******************************************"
+  ui_print "*******************************************"
+  ui_print "      小米6 MIUI 10 个性化 magisk 模块       "
+  ui_print "                                           "
+  ui_print "        MIUI 10 全面屏 magisk 模块           "
+  ui_print "     anthor: ourfor  qq群： 863481960       "
+  ui_print "                                           "
+  ui_print "             个性化调教 项目地址：             "
+  ui_print " https://github.com/ourfor/MI6_Optimization "
   ui_print "*******************************************"
 }
 
